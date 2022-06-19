c799638ff89d7760ed82a1aa8d2e4e269d92889d
Martin Guzman <mlguzmanc@tigo.com.gt>
2022-06-18 22:15:58 -0600

    simplificacion de metodo para scrapear wikipedia

--- tarea2/spiders/pagination.py
+++ tarea2/spiders/pagination.py
@@ -1,54 +1,72 @@
 import scrapy
 from scrapy.selector import Selector
+from bs4 import BeautifulSoup
+import re
 
 class PaginationSpider(scrapy.Spider):
     name = 'pagination'
     allowed_domains = ['wikipedia.org']
-    start_urls = ['https://es.wikipedia.org/wiki/Wikipedia:Portada']
-    custom_settings = {'FEEDS':{'results.json':{'format':'json', 'overwrite': True}}}
-    max_tokens = 10
+    start_urls = ["https://es.wikipedia.org/wiki/Tecnolog%C3%ADa"]
+    custom_settings = {'FEEDS':{'test1.csv':{'format':'csv', 'overwrite': True}}}
+    max_tokens = 10000
+    current_tokens = 0
+    
+    def __init__(self, *args, **kwargs):
+        super(PaginationSpider, self).__init__(*args, **kwargs)
+        if 'tokens' in kwargs:
+            tokens = int(kwargs.get('tokens'))
+            self.max_tokens = tokens
 
     def parse(self, response):
-        # entry point
-        yield from response.follow_all(css='[id="main-tfa"] h2 span a::text', callback=self.parseArticle)
-            
-    def parseArticle(self, response, current_tokens=0):
-        title = response.css('[id="firstHeading"]::text').extract_first().strip()
-        body = response.css('div.mw-parser-output > *').extract()
-        result = self.parseRecursive({'level': 1, 'title': title, 'children': []}, body)
-        yield result
-        # reviews = response.css('div[data-test-target="reviews-tab"] div[data-test-target="HR_CC_CARD"]').getall()
-        # hotel_name = response.css('[id="HEADING"]::text').extract_first()
-        # for review in reviews:
-        #     if (current_reviews < self.max_reviews_per_hotel): 
-        #         current_reviews +=1
-        #         yield self.parseReview(review, hotel_name, response.url)
+        
+        if self.current_tokens > self.max_tokens:
+            return
+        
+        # extract article content
+        html_body = response.css('div.mw-parser-output').extract_first()
+        
+        # remove html content with Beautiful soup library
+        text = BeautifulSoup(html_body,'lxml').get_text()
+        text = re.sub('\n+','. ',text)
+        text = re.sub(' +',' ',text).replace('[editar]','').strip()
+        
+        tokens = text.split(' ')
+        
+        if self.current_tokens + len(tokens) > self.max_tokens:
+            partial_text = ' '.join(tokens[0:self.max_tokens-self.current_tokens])
+            self.current_tokens = self.max_tokens
+            yield {'text':partial_text}
+        else:
+            self.current_tokens += len(tokens)
+            yield {'text':text}
+        
+        self.log(f'************************************** current tokens:{self.current_tokens}')
 
-        # href_ = response.xpath("//a[contains(@class, 'ui_button') and contains(@class, 'nav') and contains(@class, 'next') and contains(@class, 'primary')]/@href").extract_first()
-        # if (current_reviews < self.max_reviews_per_hotel and href_ != None):
-        #     yield response.follow(href_, callback=self.parseHotel, cb_kwargs={'current_reviews': current_reviews})
+        if self.current_tokens < self.max_tokens:
+            # search new urls
+            link_list = []
+            for a in response.css('div.mw-parser-output a'):
+                tmp_url = a.xpath('.//@href').get()
+                # search only for wikipedia links
+                if tmp_url!=None and tmp_url.startswith("/wiki/"):
+                    # discard images
+                    if re.match( r'.*\.(?:gif|jpg|jpeg|tiff|png|svg)$', tmp_url, re.M|re.I):
+                        continue
+                    # discard files
+                    if 'archivo:' in tmp_url.lower():
+                        continue
+                    # discard ISBN book searchs
+                    if 'especial:fuentesdelibros' in tmp_url.lower():
+                        continue
+                    link_list.append(tmp_url)
+            # remove duplicate links
+            link_list = list(set(link_list))
+
+            for link in link_list:
+                #self.log(link)
+                if self.current_tokens > self.max_tokens:
+                    break
+                yield response.follow(url=link, callback=self.parse)
+                #yield scrapy.Request(url='https://en.wikipedia.org'+link, callback=self.parse)
     
-    def parseRecursive(self, current_object, children):
-        while len(children) > 0:
-            if (not children[0].startswith('<p') and not children[0].startswith('<h')):
-                deleted = children.pop(0)
-                continue
-            
-            if (children[0].startswith('<p')):
-                child = children.pop(0)
-                child_object = Selector(text=child)
-                child_text = ' '.join(child_object.css('p::text').extract())
-                current_object['children'].append(child_text)
-            elif (children[0].startswith('<h')): 
-                level = children[0][2:3]
-                self.log(f'level: {level}')
-                level = int(level)
-                if (current_object['level'] < level):
-                    child = children.pop(0)
-                    child_object = Selector(text=child)
-                    child_text = child_object.css(f'h{level}::text').extract_first()
-                    children_object = self.parseRecursive({'title': child_text, 'level': level, 'children': []}, children)
-                    current_object['children'].append(children_object)
-                else:
-                    return current_object
-        return current_object
+          
